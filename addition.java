b71aad3491ee8dd1f38bc6fad868e575045b5831
Anjali Fukat <anjalifukat96@gmail.com>
2026-06-19 16:42:11 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,16 @@
+
+class addition {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        int d = a - b;
+        int mul = a * b;
+        int div = a / b;
+        System.out.println("Addition = " + c);
+        System.out.println("Substraction = " + d);
+        System.out.println("Multiplication = " + mul);
+        System.out.println("Division = " + div);
+    }
+}
