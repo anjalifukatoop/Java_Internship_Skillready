640ccf2702938a794c4833e9712bec790cdc90de
Anjali Fukat <anjalifukat96@gmail.com>
2026-06-19 19:39:45 +0530

    Add files via upload

---
+++ rulesofvariable.java
@@ -0,0 +1,18 @@
+// 1) do not start a variables as a digit or number
+// for ex 123abc=10 1a=20;
+//2) starting of the variable always from alphabets or underscore
+// for example int _a=10 int data=20; int _1=100;
+// 3) their is no space in variable
+// space are not allowed in the variable declaration
+// sting my name = "anjali"
+// string my_name="anjali"
+class rulesofvariable{
+    public static void main(String[] args){
+    int abc123=100;
+    int myRollNumber=123;
+    int _data=12345;
+    System.out.println(abc123);
+    System.out.println(myRollNumber);
+    System.out.println(_data);
+    }
+}
