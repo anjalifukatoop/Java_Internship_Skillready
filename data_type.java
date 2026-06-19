43ed7e6449c8d245c04e5aec32c4f8476809c9ba
Anjali Fukat <anjalifukat96@gmail.com>
2026-06-19 19:46:58 +0530

    Add files via upload

---
+++ data_type.java
@@ -0,0 +1,11 @@
+class data_type{
+    public static void main(String[] args){
+        int age=15;//Integer data type
+        float salary=25000.500f;// float data type decimal point used
+        double pi=3.14156789;//double data type
+        char grade='A';//character data type
+        boolean pass=true;//boolean data type
+        String my_name="anjali";// string data type
+
+    }
+}
