cc90be0e4f9da187ed840d2bc29b332daec521b3
Anjali Fukat <anjalifukat96@gmail.com>
2026-06-23 21:30:22 +0530

    Add files via upload

---
+++ CelsiusToFahrenheitConverter.java
@@ -0,0 +1,12 @@
+//Formula:- Fahrenheit = (Celsius * 9/5)+32;
+import java.util.Scanner;
+public class CelsiusToFahrenheitConverter{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double celsius = sc.nextDouble();
+        double fahrenheit = (celsius*9/5)+32;
+        System.out.println(fahrenheit + "Fahrenheit");
+        sc.close();
+
+    }
+}
