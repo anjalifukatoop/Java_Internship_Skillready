36d103b8f7c3e70ed07fd4c3eed7e0ed0ead4970
Anjali Fukat <anjalifukat96@gmail.com>
2026-06-23 21:56:37 +0530

    Add files via upload

---
+++ RelationalOP.java
@@ -0,0 +1,14 @@
+//compair the two values and provide the result
+//as a true or false
+// == !=not equal > < >= <=
+class RelationalOP{
+    public static void main(String[] args) {
+        int a=20;
+        int b=10;
+        System.out.println("a < b ="+(a<b));
+        System.out.println("a > b ="+(a>b));
+        System.out.println("not equal ");
+        //if(a!=b){}
+
+    }
+}
