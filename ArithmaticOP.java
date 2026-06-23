971020e6536053e51da957d5f32978e3f146bddd
Anjali Fukat <anjalifukat96@gmail.com>
2026-06-23 21:55:31 +0530

    Add files via upload

---
+++ ArithmaticOP.java
@@ -0,0 +1,12 @@
+class ArithmaticOP{
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        System.out.println("Addition = "+(a+b));
+        System.out.println("Substa = "+(a-b));
+        System.out.println("mul = "+(a*b));
+        System.err.println("div = "+(a/b));
+        System.out.println("Remainder = "+(a%b));
+        
+    }
+}
