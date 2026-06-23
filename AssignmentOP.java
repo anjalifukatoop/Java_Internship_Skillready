cf28fc2d8110771cbae299911a4e7e3e1a7816c9
Anjali Fukat <anjalifukat96@gmail.com>
2026-06-23 21:56:04 +0530

    Add files via upload

---
+++ AssignmentOP.java
@@ -0,0 +1,9 @@
+//assign the values in variable
+//= += -= *= /= %=
+class AssignmentOP{
+    public static void main(String[] args) {
+        int a=10;
+        a += 5;
+        System.out.println("value of a = "+a);//15
+    }
+}
