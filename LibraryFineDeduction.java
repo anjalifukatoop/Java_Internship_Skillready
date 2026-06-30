ac266931337e83413318707289e47b920d685833
Anjali Fukat <anjalifukat96@gmail.com>
2026-06-30 21:43:24 +0530

    Add files via upload

---
+++ LibraryFineDeduction.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class LibraryFineDeduction{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int balance = 1000;
+        int fine = sc.nextInt();
+        balance = fine;
+        System.out.println("Reamaining Balance = ₹"+ balance);
+        
+    }
+}
