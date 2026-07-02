8e539ce3965e47df80b6d4393422610ff250c47b
Anjali Fukat <anjalifukat96@gmail.com>
2026-07-02 14:59:59 +0530

    Add files via upload

---
+++ TernaryOP4.java
@@ -0,0 +1,9 @@
+class TernaryOP4{
+    public static void main(String[] args) {
+        //check employee bonus
+        int emp_expr=6;
+        String result=(emp_expr>5)?"Bonus eligible":"not eligible for bonus";
+        System.out.println("check employee "+result);
+
+    }
+}
