79f437aca27d3d3ae07834b4035399effef03843
Anjali Fukat <anjalifukat96@gmail.com>
2026-07-02 14:57:18 +0530

    Add files via upload

---
+++ TernaryOP3.java
@@ -0,0 +1,10 @@
+class TernaryOP3{
+    public static void main(String[] args) {
+        //to check number is +ve or -ve
+        int number=-10;
+        String result=(number>=0)?"number is positive":"number is negative";
+        System.out.println("check "+result);
+
+
+    }
+}
