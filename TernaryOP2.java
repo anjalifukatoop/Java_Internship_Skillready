ebcec64a121f0f3abf930a05eb0d199e659885dc
Anjali Fukat <anjalifukat96@gmail.com>
2026-07-02 14:56:36 +0530

    Add files via upload

---
+++ TernaryOP2.java
@@ -0,0 +1,9 @@
+class TernaryOP2{
+    public static void main(String[] args) {
+        //voting eligiblity
+        int age=17;
+        String result=(age>18)?"user is eligible for voting":"Not eligible";
+        System.out.println("voting eligiblity "+result);
+
+    }
+}
