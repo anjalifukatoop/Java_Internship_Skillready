56d83a1727538cbcbce846c4312c9021f1d91082
Anjali Fukat <anjalifukat96@gmail.com>
2026-07-04 22:39:24 +0530

    Add files via upload

---
+++ instanceofOP.java
@@ -0,0 +1,11 @@
+//instanceof operator checks whether an object
+//blongs to a specific class or not
+class instanceofOP{
+    public static void main(String[] args) {
+        String course= "JAVA";
+        String b="";
+        int a=10;
+        System.out.println(course instanceof String);
+        System.out.println(b instanceof String);
+    }
+}
